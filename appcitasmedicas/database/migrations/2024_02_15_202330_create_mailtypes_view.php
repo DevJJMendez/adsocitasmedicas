43a30d721b7e03c57cb1b89c5c4b2350a3dcdccc
DevJJMendez <devjjmendez@gmail.com>
2024-02-15 15:31:05 -0500

    feature database migrations
    adds all the views, working properly.

---
+++ appcitasmedicas/database/migrations/2024_02_15_202330_create_mailtypes_view.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::statement(
+            'CREATE VIEW `mailtypes_view` AS
+            SELECT
+                h.header_id AS `statu_id`,
+                d.detail_id AS `detail_id`,
+                d.value AS `statu`,
+                d.nomenclature AS `nomenclature`
+            FROM
+                `headers` AS `h`
+                JOIN
+                `details` AS `d`
+                ON
+                `h`.`header_id` = `d`.`id_header`
+                WHERE
+                `h`.`header_id` = 6;
+                ');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mailtypes_view');
+    }
+};
