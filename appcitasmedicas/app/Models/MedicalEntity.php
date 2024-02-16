938e97f38ed1a9d2c25e11ccbe66c8b6c2122052
DevJJMendez <devjjmendez@gmail.com>
2024-02-16 09:58:19 -0500

    feature / database
    adds model and migration for Medical Entity

---
+++ appcitasmedicas/app/Models/MedicalEntity.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MedicalEntity extends Model
+{
+    use HasFactory;
+    protected $table='medical_entities';
+}
