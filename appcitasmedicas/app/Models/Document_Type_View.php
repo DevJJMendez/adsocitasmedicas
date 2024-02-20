79da4551f0fb96a8653ca68cd57c4e2993cfed25
DevJJMendez <devjjmendez@gmail.com>
2024-02-20 15:35:20 -0500

    feature database
    Create Models and Migrations

---
+++ appcitasmedicas/app/Models/Document_Type_View.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Document_Type_View extends Model
+{
+    use HasFactory;
+    protected $table='document_type_views';
+}
---
+++ appcitasmedicas/app/Models/Header.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Header extends Model
+{
+    use HasFactory;
+    protected $table='headers';
+    public function details(){
+        return $this->hasMany(Detail::class,'id_header','detail_id');
+    }
+}
