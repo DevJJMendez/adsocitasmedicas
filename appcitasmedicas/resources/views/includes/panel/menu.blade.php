94aa05c1d77ecc7d8e62bdb5366370aa8f00279a
DevJJMendez <DevJJMendez@gmail.com>
2024-03-18 11:39:11 -0500

    style(citas): elimina espaciados de codigo inncesarios.

--- appcitasmedicas/resources/views/includes/panel/menu.blade.php
+++ appcitasmedicas/resources/views/includes/panel/menu.blade.php
@@ -5,22 +5,18 @@
 <h6 class="navbar-heading text-muted">Opciones Doctores</h6>
 @elsecan('Paciente')
 <h6 class="navbar-heading text-muted">Opciones Pacientes</h6>
-
 @endcan
-
 <ul class="navbar-nav">
     @can('Administrador')
         <li class="nav-item  active ">
             <a class="nav-link   " href="{{ route('asignar.index') }}">
                 <i class="fas fa-user-cog text-black"></i> Usuarios
-
             </a>
         </li>
 
         <li class="nav-item  active ">
             <a class="nav-link   " href="{{ url('/roles') }}">
                 <i class="fas fa-user-cog text-black"></i> Lista de roles
-
             </a>
         </li>
         <li class="nav-item  active ">
@@ -28,15 +24,10 @@
                 <i class="fas fa-key"></i> Permisos
             </a>
         </li>
-
-
     </ul>
-
-
     <h6 class="navbar-heading text-muted">Opciones</h6>
     <!-- Navigation -->
     <ul class="navbar-nav">
-
         <li class="nav-item  active ">
             <a class="nav-link  active " href="">
                 <i class="ni ni-tv-2 text-danger"></i> Dashboard
@@ -58,45 +49,28 @@
                 <i class="fas fa-bed text-warning"></i> Entidades Médicas
             </a>
         </li>
-        {{-- <li class="nav-item">
-            <a class="nav-link " href="{{ route('specialtyView') }}">
-                <i class="ni ni-briefcase-24 text-blue"></i> Especialidades
-            </a>
-        </li> --}}
     @endcan
 
     @can('Doctor')
-
-
         <li class="nav-item  active ">
-            <a class="nav-link   " href="">
+            <a class="nav-link" href="">
                 <i class="far fa-calendar-alt"></i> Citas Agendadas
-
             </a>
         </li>
     @endcan
-
-
     @can('Paciente')
-
-
         <li class="nav-item  active ">
-            <a class="nav-link   " href="">
-                <i class="ni ni-calendar-grid-58"></i> Mis citas
-
+            <a class="nav-link" href="">
+                <i class="ni ni-calendar-grid-58"></i> Agendar Cita
+            </a>
+        </li>
+        <li class="nav-item  active ">
+            <a class="nav-link" href="{{ route('citas.view') }}">
+                <i class="ni ni-calendar-grid-58"></i> Citas Agendadas
             </a>
         </li>
     @endcan
-    <li class="nav-item">
-        <a class="nav-link " href="{{ route('citas.view') }}">
-            <i class="fas fa-bed text-warning"></i> Citas
-        </a>
-    </li>
-
-
-
-
-
+{{-- logout --}}
 <li class="nav-item">
     <a class="nav-link" href="{{ route('citas.view') }}"
         onclick="event.preventDefault(); document.getElementById('formLogout').submit();">
@@ -107,8 +81,6 @@
     </form>
 </li>
 </ul>
-
-
 <!-- Divider -->
 <hr class="my-3">
 <!-- Heading -->
