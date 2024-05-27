885ee45bb511eafd559a284d1226712e26de4419
DevJJMendez <DevJJMendez@gmail.com>
2024-05-27 10:31:29 -0500

    Arregla error en el login

--- appcitasmedicas/app/Http/Controllers/AsignarRol.php
+++ appcitasmedicas/app/Http/Controllers/AsignarRol.php
@@ -10,49 +10,32 @@ use Spatie\Permission\Models\Role;
 
 class AsignarRol extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-
-
-     public function __construct()
-     {
-         $this->middleware('can:Administrador');
-
-
-     }
+    public function __construct()
+    {
+        $this->middleware('can:Administrador');
+    }
     public function index()
     {
-
         // $roles = Role::all();
         // $users = $roles->users()->with('thirdDataUser')->get();
         $usersWithRoles = User::with('roles')->get();
         // Dd($usersWithRoles);
-        return view('acceso.listUser' , compact('usersWithRoles'));
+        return view('acceso.listUser', compact('usersWithRoles'));
     }
-
-
-    public function edit(User  $user)
+    public function edit(User $user)
     {
-
-         $roles = Role::all();
-
-         return view('acceso.UserRole' ,compact('user' , 'roles'));
+        $roles = Role::all();
+        return view('acceso.UserRole', compact('user', 'roles'));
     }
-
     public function update(Request $request, User $user)
     {
-
-       $user->roles()->sync($request->roles);
-        return redirect()->route('asignar.edit',$user);
+        $user->roles()->sync($request->roles);
+        return redirect()->route('asignar.edit', $user);
     }
-
     public function destroy(Role $role)
     {
-        
-        notify()->error("El rol ha sido eliminado satisfactoriamente" , "Eliminar rol");
+
+        notify()->error("El rol ha sido eliminado satisfactoriamente", "Eliminar rol");
         return back();
-
     }
-
 }
